565184a7526ad55e1dd96e1d2022da126a4324e4
douzi <914954480@qq.com>
2021-05-21 19:29:30 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,45 @@
+# flutter 安卓桌面小部件
+
+## 使用插件 
+home_widget: ^0.1.2+1
+workmanager: ^0.2.2
+
+## 步骤
+1. 创建 android/app/res/layout/example_layout.xml 文件
+    - 创建小部件布局
+2. 创建 android/app/res/xml/ home_widget_example.xml 文件
+    ```
+	<?xml version="1.0" encoding="utf-8"?>
+	<appwidget-provider xmlns:android="http://schemas.android.com/apk/res/android"
+    android:minWidth="40dp"
+    android:minHeight="40dp"
+    android:updatePeriodMillis="86400000"
+    android:initialLayout="@layout/example_layout"
+    android:resizeMode="horizontal|vertical"
+    android:widgetCategory="home_screen">
+	</appwidget-provider>
+	
+3. 添加 WidgetReceiver到AndroidManifest.xml
+	```
+	<receiver android:name="HomeWidgetExampleProvider" >
+    <intent-filter>
+        <action android:name="android.appwidget.action.APPWIDGET_UPDATE" />
+    </intent-filter>
+    <meta-data android:name="android.appwidget.provider"
+        android:resource="@xml/home_widget_example" />
+		</receiver>
+		
+4. 创建 kotlin/com/example/fluttertest/HomeWidgetExampleProvider.kt
+
+
+| 名称 | 测试结果 |
+| -- | -- |
+| 安卓 | ··|
+| 小部件显示排版 | ✅ |
+| 小部件点击进入 | ✅ |
+| 小部件按钮点击 | ❎ |
+| Ios | ·· |
+| 未测试 | ❎ |
+
+
+![](http://img1.kllh.vip/981621596388_.pic.jpg)
---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:home_widget/home_widget.dart';
+import 'package:workmanager/workmanager.dart';
+
+/// Used for Background Updates using Workmanager Plugin
+void callbackDispatcher() {
+  Workmanager.executeTask((taskName, inputData) {
+    final now = DateTime.now();
+    return Future.wait<bool>([
+      HomeWidget.saveWidgetData(
+        'title',
+        'Updated from Background',
+      ),
+      HomeWidget.saveWidgetData(
+        'message',
+        '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}',
+      ),
+      HomeWidget.updateWidget(
+        name: 'HomeWidgetExampleProvider',
+        iOSName: 'HomeWidgetExample',
+      ),
+    ]).then((value) {
+      return !value.contains(false);
+    });
+  });
+}
+
+/// Called when Doing Background Work initiated from Widget
+void backgroundCallback(Uri data) async {
+  print(data);
+
+  if (data.host == 'titleclicked') {
+    final greetings = [
+      'Hello',
+      'Hallo',
+      'Bonjour',
+      'Hola',
+      'Ciao',
+      '哈洛',
+      '안녕하세요',
+      'xin chào'
+    ];
+    final selectedGreeting = greetings[Random().nextInt(greetings.length)];
+
+    await HomeWidget.saveWidgetData<String>('title', selectedGreeting);
+    await HomeWidget.updateWidget(
+        name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+  }
+}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager.initialize(callbackDispatcher, isInDebugMode: kDebugMode);
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    HomeWidget.setAppGroupId('YOUR_GROUP_ID');
+    HomeWidget.registerBackgroundCallback(backgroundCallback);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _checkForWidgetLaunch();
+    HomeWidget.widgetClicked.listen(_launchedFromWidget);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _sendData() async {
+    try {
+      return Future.wait([
+        HomeWidget.saveWidgetData<String>('title', _titleController.text),
+        HomeWidget.saveWidgetData<String>('message', _messageController.text),
+      ]);
+    } on PlatformException catch (exception) {
+      debugPrint('Error Sending Data. $exception');
+    }
+  }
+
+  Future<void> _updateWidget() async {
+    try {
+      return HomeWidget.updateWidget(
+          name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+    } on PlatformException catch (exception) {
+      debugPrint('Error Updating Widget. $exception');
+    }
+  }
+
+  Future<void> _loadData() async {
+    try {
+      return Future.wait([
+        HomeWidget.getWidgetData<String>('title', defaultValue: 'Default Title')
+            .then((value) => _titleController.text = value),
+        HomeWidget.getWidgetData<String>('message',
+                defaultValue: 'Default Message')
+            .then((value) => _messageController.text = value),
+      ]);
+    } on PlatformException catch (exception) {
+      debugPrint('Error Getting Data. $exception');
+    }
+  }
+
+  Future<void> _sendAndUpdate() async {
+    await _sendData();
+    await _updateWidget();
+  }
+
+  void _checkForWidgetLaunch() {
+    print('------');
+        HomeWidget.initiallyLaunchedFromHomeWidget().then(
+          _launchedFromWidget);
+  }
+
+  void _launchedFromWidget(Uri uri) {
+    print(uri);
+    if (uri != null) {
+      showDialog(
+          context: context,
+          builder: (buildContext) => AlertDialog(
+                title: Text('App started from HomeScreenWidget'),
+                content: Text('Here is the URI: $uri'),
+              ));
+    }
+  }
+
+  void _startBackgroundUpdate() {
+    Workmanager.registerPeriodicTask('1', 'widgetBackgroundUpdate',
+        frequency: Duration(minutes: 15));
+  }
+
+  void _stopBackgroundUpdate() {
+    Workmanager.cancelByUniqueName('1');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeWidget Example'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Title',
+              ),
+              controller: _titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Body',
+              ),
+              controller: _messageController,
+            ),
+            ElevatedButton(
+              onPressed: _sendAndUpdate,
+              child: Text('Send Data to Widget'),
+            ),
+            ElevatedButton(
+              onPressed: _loadData,
+              child: Text('Load Data'),
+            ),
+            ElevatedButton(
+              onPressed: _checkForWidgetLaunch,
+              child: Text('Check For Widget Launch'),
+            ),
+            if (Platform.isAndroid)
+              ElevatedButton(
+                onPressed: _startBackgroundUpdate,
+                child: Text('Update in background'),
+              ),
+            if (Platform.isAndroid)
+              ElevatedButton(
+                onPressed: _stopBackgroundUpdate,
+                child: Text('Stop updating in background'),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
